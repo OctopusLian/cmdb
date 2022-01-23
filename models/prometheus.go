39ca917313777ce4bc63ddc87f9869917657c6a4
OctopusLian <zoctopus@qq.com>
2022-01-23 14:50:15 +0800

    注释prometheus

--- models/prometheus.go
+++ models/prometheus.go
@@ -3,50 +3,44 @@
  * @Author: neozhang
  * @Date: 2022-01-12 23:25:48
  * @LastEditors: neozhang
- * @LastEditTime: 2022-01-13 12:57:01
+ * @LastEditTime: 2022-01-23 14:50:04
  */
 package models
 
-import (
-	"time"
+// type Node struct {
+// 	ID        int
+// 	UUID      string
+// 	Hostname  string
+// 	Addr      string
+// 	CreatedAt *time.Time `orm:"auto_now_add"`
+// 	UpdatedAt *time.Time `orm:"auto_now"`
+// 	DeletedAt *time.Time `orm:"null"`
+// 	Jobs      []*Job
+// }
 
-	"github.com/beego/beego/v2/adapter/orm"
-)
+// type Jobs struct {
+// 	ID        int
+// 	Key       string
+// 	Remark    string
+// 	CreatedAt *time.Time `orm:"auto_now_add"`
+// 	UpdatedAt *time.Time `orm:"auto_now"`
+// 	DeletedAt *time.Time `orm:"null"`
+// }
 
-type Node struct {
-	ID        int
-	UUID      string
-	Hostname  string
-	Addr      string
-	CreatedAt *time.Time `orm:"auto_now_add"`
-	UpdatedAt *time.Time `orm:"auto_now"`
-	DeletedAt *time.Time `orm:"null"`
-	Jobs      []*Job
-}
+// type Target struct {
+// 	ID        int
+// 	UUID      string
+// 	Hostname  string
+// 	Addr      string
+// 	CreatedAt *time.Time `orm:"auto_now_add"`
+// 	UpdatedAt *time.Time `orm:"auto_now"`
+// 	DeletedAt *time.Time `orm:"null"`
+// 	Job       *Job
+// }
 
-type Jobs struct {
-	ID        int
-	Key       string
-	Remark    string
-	CreatedAt *time.Time `orm:"auto_now_add"`
-	UpdatedAt *time.Time `orm:"auto_now"`
-	DeletedAt *time.Time `orm:"null"`
-}
+// type Job struct {
+// }
 
-type Target struct {
-	ID        int
-	UUID      string
-	Hostname  string
-	Addr      string
-	CreatedAt *time.Time `orm:"auto_now_add"`
-	UpdatedAt *time.Time `orm:"auto_now"`
-	DeletedAt *time.Time `orm:"null"`
-	Job       *Job
-}
-
-type Job struct {
-}
-
-func init() {
-	orm.RegisterModel(new(Node), new(Jobs), new(Target))
-}
+// func init() {
+// 	orm.RegisterModel(new(Node), new(Jobs), new(Target))
+// }
